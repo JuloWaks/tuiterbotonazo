e27a51edcf2ffe58dbc2f64ec7092c6303ec4569
unknown <39000634@A-CNA-01.ALMAGRO.ORT.ARG>
2013-06-05 15:02:35 -0300

    Mi primer comit

---
+++ README.txt
@@ -0,0 +1,2 @@
+http://api.twitter.com/1/users/show.json?screen_name=eljulo
+*prueba de comit*
---
+++ helo.py
@@ -0,0 +1,9 @@
+#!python
+
+print("Content-type: text/html")
+
+print("<html>")
+print("<head>")
+print("<title> Hola mundo! </title>")
+print ("</head>")
+print("<h1> HELLO WORLD </h1>")
