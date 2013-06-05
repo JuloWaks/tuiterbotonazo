22a59418cd5f04fd3086624de9b9dd67483c6763
Julian <39000634@A-CNA-01.ALMAGRO.ORT.ARG>
2013-06-05 17:28:35 -0300

    Con urllib

--- tuiterbotonazo.py
+++ tuiterbotonazo.py
@@ -1,6 +1,10 @@
 #!python3.3
 
 import sys
+import urllib.request
 
 print("Usuario: " + sys.argv[1])
 print("Email: " + sys.argv[2])
+f = urllib.request.urlopen("http://api.twitter.com/1/users/show.json?screen_name=eljulo")
+f.read()
+print(list(r.headers.items()))
