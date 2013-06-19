e6f77d5de8a308312779de8efe0d1b19e3cd4186
Julian W <Juwaks@gmail.com>
2013-06-19 16:12:31 -0300

    Se modificaron reglas del PEP8

--- tuiterbotonazo.py
+++ tuiterbotonazo.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
+
 import sys
-import os
-from twitter import OAuth,Twitter
+from twitter import OAuth, Twitter
+
 with open("tokens.cfg") as fh:
-   cfg = dict(x.split() for x in fh)
+	cfg = dict(x.split() for x in fh)
 print(cfg)
-twitter = Twitter(auth=OAuth(cfg['access_token'], cfg['access_token_secret'],cfg['consumer_key'],cfg['consumer_secret']))
+twitter = Twitter(auth=OAuth(
+		cfg['access_token'], 
+		cfg['access_token_secret'], 
+		cfg['consumer_key'], 
+		cfg['consumer_secret']))
+
 Usuario = sys.argv[1]
 print("Usuario: " + sys.argv[1])
 print("Email: " + sys.argv[2])
 f = twitter.followers.ids(screen_name= sys.argv[1])
 print("Hola %s , tenes %s seguidores" % (Usuario, len(f["ids"])))
+
+
 
 
