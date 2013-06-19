079b367d5dd71488a7bbb7e6eea686a3078b0b41
Julian W <Juwaks@gmail.com>
2013-06-19 16:18:42 -0300

    algunas cosas mas de PEP8

--- tuiterbotonazo.py
+++ tuiterbotonazo.py
@@ -4,18 +4,17 @@ import sys
 from twitter import OAuth, Twitter
 
 with open("tokens.cfg") as fh:
-	cfg = dict(x.split() for x in fh)
+    cfg = dict(x.split() for x in fh)
 print(cfg)
 twitter = Twitter(auth=OAuth(
-		cfg['access_token'], 
-		cfg['access_token_secret'], 
-		cfg['consumer_key'], 
-		cfg['consumer_secret']))
-
+    cfg['access_token'], 
+    cfg['access_token_secret'],
+    cfg['consumer_key'],
+    cfg['consumer_secret']))
 Usuario = sys.argv[1]
 print("Usuario: " + sys.argv[1])
 print("Email: " + sys.argv[2])
-f = twitter.followers.ids(screen_name= sys.argv[1])
+f = twitter.followers.ids(screen_name=sys.argv[1])
 print("Hola %s , tenes %s seguidores" % (Usuario, len(f["ids"])))
 
 
