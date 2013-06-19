09d67685d82c4f318443c1b94c92cb089abeb90d
Julian W <Juwaks@gmail.com>
2013-06-19 16:32:17 -0300

    Se agregaron comentarios al codigo

--- tuiterbotonazo.py
+++ tuiterbotonazo.py
@@ -5,18 +5,19 @@ from twitter import OAuth, Twitter
 
 with open("tokens.cfg") as fh:
     cfg = dict(x.split() for x in fh)
-print(cfg)
+  # Getting the tokens on the file "tokens.cfg"
+
 twitter = Twitter(auth=OAuth(
     cfg['access_token'], 
     cfg['access_token_secret'],
     cfg['consumer_key'],
-    cfg['consumer_secret']))
-Usuario = sys.argv[1]
+    cfg['consumer_secret']))  # Getting the authentication requiered by Twitter
+
+username = sys.argv[1]  # Saving username
+
 print("Usuario: " + sys.argv[1])
 print("Email: " + sys.argv[2])
-f = twitter.followers.ids(screen_name=sys.argv[1])
-print("Hola %s , tenes %s seguidores" % (Usuario, len(f["ids"])))
 
+f = twitter.followers.ids(screen_name=username)  # Getting user followers ids
 
-
-
+print("Hola %s , tenes %s seguidores" % (username, len(f["ids"])))
