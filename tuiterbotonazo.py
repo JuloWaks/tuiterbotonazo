0b1caeeead8da0d731b90571f8ccfb6eecb28fc5
Julian W <Juwaks@gmail.com>
2013-06-28 01:01:08 -0300

    Se sumaron comentarios sobre el codigo agregado

--- tuiterbotonazo.py
+++ tuiterbotonazo.py
@@ -19,7 +19,7 @@ twitter = Twitter(auth=OAuth(
     cfg['consumer_key'],
     cfg['consumer_secret']))  # Getting the authentication requiered by Twitter
 
-username, email = sys.argv[1].lower(), sys.argv[2].lower()  # Saving username
+username, email = sys.argv[1].lower(), sys.argv[2].lower()  # Saving username and email
 
 
 print("Usuario: %s" % username)
@@ -39,29 +39,37 @@ while f["next_cursor"] != 0:
     new_ids.extend(list(str(idd).strip() for idd in f["ids"]))
 
 if path.isfile("%s.flwrs" % username):
+    # If the user has already use the script
     with open("%s.flwrs" % username) as slist:
         old_ids = list(idd.strip() for idd in slist)
 
     set_old_ids = set(old_ids)
     set_new_ids = set(new_ids)
+    # Converting lists to sets
+
     lst_unflws = list(set_old_ids.difference(set_new_ids))
     lst_flws = list(set_new_ids.difference(set_old_ids))
+    # Getting the differnce of the sets
 
     print("Te comenzaron a seguir %s personas, y dejaron de seguirte %s personas"
         % (len(lst_flws), len(lst_unflws)))
-    d_unflws = dict()
+    # Printing the counts of follows and unfollows
+    d_unflws = dict()  # Initalizing dictionaries, not sure very pythonic
     d_flws = dict()
     for idk in lst_unflws:
         r_unflws = twitter.users.show(user_id=idk)
         d_unflws[idk] = r_unflws["screen_name"]
+        # Getting the name of the unfollower
     for idd in lst_unflws:
         print("Te dejo de seguir: @%s" % (d_unflws[idk]))
+        # Printing the name of the unfollower
     for idk in lst_flws:
         r_flws = twitter.users.show(user_id=idk)
         d_flws[idk] = r_flws["screen_name"]
+        # Getting the name of the NEW follower
     for idd in lst_flws:
         print("Te comenzo de seguir: @%s" % (d_flws[idk]))
-
+        # Printing the name of the NEW follower
 with open("%s.flwrs" % username, "w") as slist:
 
     for idd in new_ids:
