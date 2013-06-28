e308eebc2586f8dd3008c9321b751d87de1836a8
Julian W <Juwaks@gmail.com>
2013-06-28 00:54:09 -0300

    Se agrego la comparación de listas, a pesar de crer que la manera en la cual se hace no es muy pythonesca

--- tuiterbotonazo.py
+++ tuiterbotonazo.py
@@ -4,18 +4,17 @@ import sys
 from twitter import OAuth, Twitter
 from os import path
 
-while len(sys.argv) != 3:
+if len(sys.argv) != 3:   # Validation
     print("Recuerda que debes escribir python3 tuiterbotonazo.py TuUsuario TuEmail")
     exit()
-  #Validation
 
-with open("tokens.cfg") as fh:
+
+with open("tokens.cfg") as fh:  # Getting the tokens on the file "tokens.cfg"
     cfg = dict(x.split() for x in fh)
 
-  # Getting the tokens on the file "tokens.cfg"
 
 twitter = Twitter(auth=OAuth(
-    cfg['access_token'], 
+    cfg['access_token'],
     cfg['access_token_secret'],
     cfg['consumer_key'],
     cfg['consumer_secret']))  # Getting the authentication requiered by Twitter
@@ -25,22 +24,48 @@ username, email = sys.argv[1].lower(), sys.argv[2].lower()  # Saving username
 
 print("Usuario: %s" % username)
 print("Email: %s" % email)
-if path.isfile("%s.flwrs" % username):    
-    with open("%s.flwrs" % username) as slist:
-        ids = list(idd.strip() for idd in slist)        
-else:
-    f = twitter.followers.ids(screen_name=username)  
+
+
+f = twitter.followers.ids(screen_name=username)
 # Getting user followers ids
-    ids = f["ids"]
+new_ids = list(str(idd).strip() for idd in f["ids"])
 
-    while f["next_cursor"] != 0:
-        f = twitter.followers.ids(screen_name=username,
-                                 cursor=f['next_cursor'])
-        ids.extend(f["ids"])  # If the user have more than 5000 followers
 
-    with open("%s.flwrs" % username, "w") as slist: 
-# Saving the list of followers for further comparing
-        for idd in ids:
-            slist.write("%s\n" % idd)
+while f["next_cursor"] != 0:
+    # If the user have more than 5000 followers
+    f = twitter.followers.ids(
+        screen_name=username,
+        cursor=f['next_cursor'])
+    new_ids.extend(list(str(idd).strip() for idd in f["ids"]))
 
-print("Hola %s , tenes %s seguidores" % (username, len(ids)))
+if path.isfile("%s.flwrs" % username):
+    with open("%s.flwrs" % username) as slist:
+        old_ids = list(idd.strip() for idd in slist)
+
+    set_old_ids = set(old_ids)
+    set_new_ids = set(new_ids)
+    lst_unflws = list(set_old_ids.difference(set_new_ids))
+    lst_flws = list(set_new_ids.difference(set_old_ids))
+
+    print("Te comenzaron a seguir %s personas, y dejaron de seguirte %s personas"
+        % (len(lst_flws), len(lst_unflws)))
+    d_unflws = dict()
+    d_flws = dict()
+    for idk in lst_unflws:
+        r_unflws = twitter.users.show(user_id=idk)
+        d_unflws[idk] = r_unflws["screen_name"]
+    for idd in lst_unflws:
+        print("Te dejo de seguir: @%s" % (d_unflws[idk]))
+    for idk in lst_flws:
+        r_flws = twitter.users.show(user_id=idk)
+        d_flws[idk] = r_flws["screen_name"]
+    for idd in lst_flws:
+        print("Te comenzo de seguir: @%s" % (d_flws[idk]))
+
+with open("%s.flwrs" % username, "w") as slist:
+
+    for idd in new_ids:
+        # Saving the list of followers for further comparing
+        slist.write("%s\n" % idd)
+
+print("Hola %s , tenes %s seguidores" % (username, len(new_ids)))
