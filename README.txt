eef55a89d576533908a8028677736fde3c24368e
Julian W <Juwaks@gmail.com>
2013-07-05 22:24:46 -0300

    Elimando archivos extra

--- README.txt
+++
@@ -1,6 +0,0 @@
-http://api.twitter.com/1/users/show.json?screen_name=eljulo
-*prueba de comit*
-
-*esto es mi segundo cmit*
-
-*este es parte del segundo*
--- helo.py
+++
@@ -1,9 +0,0 @@
-#!python
-
-print("Content-type: text/html")
-
-print("<html>")
-print("<head>")
-print("<title> Hola mundo! </title>")
-print ("</head>")
-print("<h1> HELLO WORLD </h1>")
